30f38932cfa5375af716adff95862a60c85112f8
Clinton Johnson <dragoon1246224@gmail.com>
2018-11-25 19:23:19 -0500

    Updates

---
+++ precache-manifest.125900d808c495adab9673b9c8de394b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb5d57de38284f0a3d03",
+    "url": "/todos-part-3/static/js/runtime~main.eb5d57de.js"
+  },
+  {
+    "revision": "54f748d6ef4ee4694379",
+    "url": "/todos-part-3/static/js/main.54f748d6.chunk.js"
+  },
+  {
+    "revision": "1b1ae531052b102d614c",
+    "url": "/todos-part-3/static/js/1.1b1ae531.chunk.js"
+  },
+  {
+    "revision": "54f748d6ef4ee4694379",
+    "url": "/todos-part-3/static/css/main.3717e356.chunk.css"
+  },
+  {
+    "revision": "781317710b177cb0bfe96e4067129d99",
+    "url": "/todos-part-3/index.html"
+  }
+];
